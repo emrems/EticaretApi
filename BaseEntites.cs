cb8fe421d7e1fb5de15fbdb561867243ca482c5f
emre almamis <ealmamis56@gmail.com>
2025-09-03 13:04:04 +0300

    Proje dosyası ekle.

---
+++ BaseEntites.cs
@@ -0,0 +1,9 @@
+﻿public abstract class BaseEntites
+{
+    public long Id { get; set; }                // Primary key
+    public DateTime CreatedDate { get; set; }  // Oluşturulma tarihi
+    public DateTime? UpdatedDate { get; set; } // Güncellenme tarihi (nullable)
+    public bool IsDeleted { get; set; }        // Soft delete için
+    public string? CreateUser { get; set; }    // Kaydı oluşturan kullanıcı
+    public string? UpUser { get; set; }        // Güncelleyen kullanıcı
+}
---
+++ Users.cs
@@ -0,0 +1,8 @@
+public class Users : BaseEntites
+{
+    public string Email { get; set; }          
+    public string Password { get; set; }        
+    public string FirstName { get; set; }           
+    public string LastName { get; set; }            
+
+}
