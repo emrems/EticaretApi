43c13a117ec27b18b32b7eedce4ecc0f26eda4ab
Aysegul-selek <aysegul.selek03@gmail.com>
2025-09-03 13:17:56 +0300

    test

--- Users.cs
+++ Users.cs
@@ -3,6 +3,9 @@ public class Users : BaseEntites
     public string Email { get; set; }          
     public string Password { get; set; }        
     public string FirstName { get; set; }           
-    public string LastName { get; set; }            
+    public string LastName { get; set; } 
+    
+
+
 
 }
